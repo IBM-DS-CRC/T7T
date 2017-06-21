6458532139798d60c3c5694f72cf36f409410d8c
093569 - Gerardo Adolfo Salas Montoya <gesalas@cr.ibm.com>
2017-06-21 10:17:37 -0600

    new comments in the dataExtraction.R and new logic for the login variables

--- dataExtraction.R
+++ dataExtraction.R
@@ -1,50 +1,55 @@
-library(XML)
+#Starting with the Library
 library(rjson)
 library(RJSONIO)
 library(httr)
 library(jsonlite)
 library(lubridate)
+#*************************
+
+#Username and Pass for the IBM Forms, methot is curl and the app is not allowing anon connections, so we are using secure connections.
+MyUser <- "User@cr.ibm.com" #From Shiny interface
+MyPass <- "HolaPapitos785+" #From Shiny interface
+
+#********************************
 
 
+#URLand Path for the IBM Forms Tool, using JSON
 url  <- "https://w3-01.ibm.com"
-path <- "/tools/cio/forms-basic/anon/org/data/bbe9380e-6976-44ef-8475-9aaac239b01f/F_Form1?format=application/json"
+path <- "/tools/cio/forms-basic/secure/org/data/bbe9380e-6976-44ef-8475-9aaac239b01f/F_Form1?format=application/json"
+#**********************************************
+
+#Extracting the data from the API using GET
+raw.result <- GET(url = url, path = path, authenticate(MyUser,MyPass))
+#**********************************************
 
 
+#Exploratory Data Analysis
+Names <- names(raw.result)
 
-raw.result <- GET(url = url, path = path)
+rawResuld <- raw.result$status_code
 
-
-names(raw.result)
-
-raw.result$status_code
-
-head(raw.result$content)
+headResult <- head(raw.result$content)
 
 this.raw.content <- rawToChar(raw.result$content)
 
 nchar(this.raw.content)
 
-substr(this.raw.content, 1, 100)
+substr(this.raw.content, 1, 135)
 
 this.content <- fromJSON(this.raw.content)
 
-class(this.content) #it's a list
+#class(this.content) 
 
-length(this.content) #it's a large list
+#length(this.content) 
 
-this.content[[4]] #the list
+this.content[[4]] #the list we need 
 
 this.content$items
 
 class(this.content$items)
+#************************************************
 
+#Creating the Data Frame in order to start cleaning the data, this variable will be used in the dataprocessing.R script
 DtFrame <- this.content$items
-
-
-
-
-
-
-
-
+#************************************************
 
