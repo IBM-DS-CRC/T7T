76400412274bd4494acd1094da2d9ed38070e691
Scott <Kevin.urbinamoreira@hotmail.com>
2017-06-22 10:05:13 -0600

    Adding column names

--- Kevin/R first project.R
+++ Kevin/R first project.R
@@ -12,10 +12,12 @@ library(httr)
 library(jsonlite)
 library(lubridate)
 library(stringi)
+library(devtools)
+
 
 #Username and Pass for the IBM Forms, methot is curl and the app is not allowing anon connections, so we are using secure connections.
-MyUser <- "kurbina@cr.ibm.com"
-MyPass <- "Holahol!@#$"
+MyUser <- "kurbina@cr.ibm.com" 
+MyPass <- "Holahol!@#$" 
 
 #********************************
 
@@ -27,7 +29,6 @@ path <- "/tools/cio/forms-basic/secure/org/data/98fdd9e5-4671-448a-8578-9a71a719
 
 #Extracting the data from the API using GET
 raw.result <- GET(url = url, path = path, authenticate(MyUser,MyPass))
-
 #**********************************************
 
 
@@ -42,7 +43,7 @@ this.raw.content <- rawToChar(raw.result$content)
 
 nchar(this.raw.content)
 
-substr(this.raw.content, 1, 135)
+substr(this.raw.content, 1, 160)
 
 this.content <- fromJSON(this.raw.content)
 
@@ -50,30 +51,201 @@ this.content <- fromJSON(this.raw.content)
 
 #length(this.content) 
 
-this.content[[4]] #the list we need 
+#this.content[[4]] #the list we need 
 
 this.content$items
 
-class(this.content$items)
 #************************************************
 
 #Creating the Data Frame in order to start cleaning the data, this variable will be used in the dataprocessing.R script
 DtFrame <- this.content$items
 #************************************************
 
-# Changing each column name
+
+
+# Changing column names
 
 colnames(DtFrame)[which(names(DtFrame) == "id")] <- "ID"
+
 colnames(DtFrame)[which(names(DtFrame) == "F_SingleLine2")] <- "PCR Number"
 
+colnames(DtFrame)[which(names(DtFrame) == "F_SingleLine5")] <- "PCR Title:"
 
-#Converting into a Data Frame
+colnames(DtFrame)[which(names(DtFrame) == "F_pcrstate")] <- "PCR State:"
 
-Form_PMO<-data.frame(Form_PMO)
+colnames(DtFrame)[which(names(DtFrame) == "F_currentstage")] <- "PCR Current Stage:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_PCRtobeCp")] <- "PCR # to Clone:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_DropDown7")] <- "Account Name:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_DropDown6")] <- "Sector:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_SingleLine1")] <- "TTIM Name:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_SingleLine11")] <- "XM Name:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_tsm")] <- "TSM Name:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_accountgeo")] <- "GEO:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_accountiot")] <- "Account IOT:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_accountimt")] <- "IMT:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_accountcountry")] <- "Country:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_Paragraphtext1")] <- "PCR Executive Summary Statement:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_DropDown1")] <- "PCR Class:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_DropDown5")] <- "PCR Priority:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_DropDown9")] <- "PCR Tower or Function:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_SingleLine10")] <- "PCR Creator:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_SingleLine8")] <- "PCR Owner:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_DropDown10")] <- "Engagement Failure Category:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_tsvr")] <- "Found in TSVR Session?"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_Date2")] <- "Estimated Start Date:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_Date3")] <- "Estimated Finish Date:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_Date1")] <- "Budget Start Date:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_Date4")] <- "Budget End Date:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_Date7")] <- "Date Identified:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_Table1")] <- "Cost Summary"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_Currency16")] <- "T&T Total:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_Currency17")] <- "SS Total (Est):"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_Currency18")] <- "PCR Total (USD):"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_subprojects")] <- "Sub-Projects:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_Currency4")] <- "T&T Non-Labor Cost"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_Currency10")] <- "T&T Labor Cost"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_Number2")] <- "T&T Labor Hours"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_Currency11")] <- "T&T Total"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_Currency13")] <- "SS Non-Labor Cost"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_Currency14")] <- "SS Labor Cost"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_Number1")] <- "SS Labor Hours"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_Currency15")] <- "SS Total"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_Currency5")] <- "Total Non-Labor"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_Currency6")] <- "Total Labor"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_Number3")] <- "PCR Total - Hours"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_Currency1")] <- "PCR Total (USD)"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_Paragraphtext3")] <- "Detailed Summary:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_Paragraphtext4")] <- "Background Information:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_Paragraphtext5")] <- "Impact Assessment:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_Attachment1")] <- "Attachment"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_Attachment2")] <- "Attachment_1"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_Attachment3")] <- "Attachment_2"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_Attachment4")] <- "Attachment_3"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_URL1")] <- "URL"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_URL2")] <- "URL_1"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_URL3")] <- "URL_2"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_URL4")] <- "URL_3"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_initiatorcomments")] <- "Initiator Comments:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_ttimcomments")] <- "TTIM Comments:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_dpecomments")] <- "DPE Comments:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_facomments")] <- "Contract FA Comments:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_solutioningcomments")] <- "Engagement Comments:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_pecomments")] <- "PE Comments:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_pfdcomments")] <- "Portfolio Director / T&T Portfolio Leader Comments:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_ivpcomments")] <- "Industry VP Comments:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_gmcomments")] <- "GM Comments:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_ttcomments")] <- "T&T VP / Director T&T Europe Comments:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_peapproval")] <- "PEs approval means s/he will or has billed the Client."
+
+colnames(DtFrame)[which(names(DtFrame) == "F_pdlcheckc2")] <- "This includes the confirmation that the new budget was HCP-D approved/certified."
+
+colnames(DtFrame)[which(names(DtFrame) == "F_pdlcheckc3")] <- "This includes the confirmation that the IMT GTS exec has provided his approval in writing."
+
+colnames(DtFrame)[which(names(DtFrame) == "F_capincreased")] <- "CAP Increased Y/N"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_dateclientagreedtopay")] <- "Signature date when client agreed to pay:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_finalamountbilled")] <- "Final Amount Billet to Client:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_clientagreedtopaycom")] <- "Payment comments:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_name")] <- "Name:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_lastname")] <- "Last Name:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_servertime")] <- "Server Time:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_linktopcr")] <- "Link"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_originalclass")] <- "Original Class:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_currentuser")] <- "Current User NAME:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_currentuseremail")] <- "Current User EMAIL:"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_recordid")] <- "RID"
+
+colnames(DtFrame)[which(names(DtFrame) == "F_ttimapprovedate")] <- "TTIM Approve Date:"
+
+
+## Needs to be updated
+
+
+
+myvars <- names(DtFrame) %in% c("lastModifiedBy", "createdBy", "Attachment",
+                            "Attachment_1","Attachment_2","Attachment_3", 
+                            "createdBy", "F_Table1", "F_Table2", "availableSubmitButtons")
+
+DtFrame <- DtFrame [,!myvars]
+
+
+
 
 #Removing signs of punctuation from headers, this will help us to filter
 
-names(Form_PMO)<- gsub("[:,' .]" ,"", names(Form_PMO))
+names(DtFrame)<- gsub("[:]" ,"", names(DtFrame))
 
 #Creating a view without blancks ID's
 
@@ -147,13 +319,10 @@ Semifinal_Dc<- rbind(InProgress_PCRS,Approved_PCRs, FirstPendingDSet, SecondPend
 
 library(xlsx)
 
-write.csv(Semifinal_Dc,"c:/mydata.csv")
+write.csv(DtFrame,"c:/mydata1.csv")
 
 
 
 ## Watson API
-
-
-URL<- https://api.ibm.com/watsonanalytics/run/oauth2/v1/config
 
 
