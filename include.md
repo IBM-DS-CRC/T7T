71afb95057a8093f04607d3970a637b70b2f2272
093569 - Gerardo Adolfo Salas Montoya <gesalas@cr.ibm.com>
2017-07-05 13:25:02 -0600

    new changes to the server and ui, and also to the include.md

---
+++ include.md
@@ -0,0 +1,26 @@
+
+
+
+# Instructions
+-------------------------------------------------
+
+## Requirements
+
+ *In order to use this application you will need:
+
+- Access to W3 account.
+- Access to TT Finance IBM Form.
+
+## Pulling the information
+
+- In order to pull out the information you will need to enter your credentials in the respective field in the left section of this page.
+
+- After you enter the credentials, you just need to click the GET REPORT buttom and the system is going to start extracting the data directly from the IBM Form.
+
+*** Note: This process is going to take some minutes, so we recommend to wait 3 to 5 minutes until the report completes.
+
+## Downloading the report
+
+If you completed correctly the previus steps, the progress box in the top of the page will change to "PROGRESS 100%" and the icon will be green, at this point you will be able to click the "Download TT Finance" buttom  an save the report in your computer.
+
+
--- ui.R
+++ ui.R
@@ -1,19 +1,29 @@
 library(shiny)
 library(shinydashboard)
+library(markdown)
 
 
 dashboardPage(
-    dashboardHeader(title = "IBM - TT Finance Report"),
-    dashboardSidebar(
-        textInput("login", "Please provide your IBM Mail"),
-        passwordInput("pass", "Please provide your IBM intranet password."),
-        actionButton("do", "Get Report"),
-        downloadButton("downloadTT", "Download TT Finance")
+  dashboardHeader(title = "IBM - TT Finance Report"),
+  dashboardSidebar(
+    
+    textInput("login", "Please provide your IBM Mail"),
+    passwordInput("pass", "Please provide your IBM intranet password."),
+    actionButton("do", "Get Report"),
+    downloadButton("downloadTT", "Download")
+  ),
+  dashboardBody(
+    # Boxes need to be put in a row (or column)
+    fluidRow(
+      infoBoxOutput("progressBox"),
+      infoBoxOutput("progressBox2")
     ),
-    dashboardBody(
-        # Boxes need to be put in a row (or column)
-        fluidRow(
-            textOutput("response")
-        )
+    fluidRow(
+      box(title = "TT Finance Instructions", status = "primary", solidHeader = TRUE,
+          collapsible = TRUE,
+          p(includeMarkdown('include.md')) #calling the Markdown File with the instructions
+      )
     )
+    
+  )
 )
